e727d29ceee0f8f7b9662d5f197cc7841e7ca5da
Wouter Bijnsdorp <wouterb98@gmail.com>
2021-04-06 10:21:49 +0200

    init models, connection string

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const pug = require('pug');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const workModels = require('./models/workMade');
+
 const app = express();
 const port = process.env.PORT;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug')
+
+// Setting up mongoose connection data like the database name, allowing it to error
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true, dbName:process.env.DB_NAME});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
 
 app.get('/',(req, res) =>{
     res.send('welcome');    
